b43ab3b57072d33c78c0a75571a9718366d2f549
fanghao <hashfangh000@163.com>
2023-06-26 18:07:34 +0800

    pattern

---
+++ src/main/java/com/example/goodoog/pattern/responsibility/LeaveRequest.java
@@ -0,0 +1,19 @@
+package com.example.goodoog.pattern.responsibility;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 请假条类
+ */
+@Getter
+@AllArgsConstructor
+public class LeaveRequest {
+
+    private String name;
+
+    private int num;
+
+    private String content;
+
+}
